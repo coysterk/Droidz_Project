3b3ed62060ba3c14d7f0c2fcd46b4ce763a0d5fc
MiketheFibber <jacobtheChristianGamer@gmail.com>
2025-12-08 16:30:08 -0500

    update

--- Assets/Scripts/Zombie Horde Help/ZombieAnimation.cs
+++ Assets/Scripts/Zombie Horde Help/ZombieAnimation.cs
@@ -2,15 +2,18 @@ using UnityEngine;
 
 public class ZombieAnimation : MonoBehaviour
 {
+    Animator anim;
+    HordeEvaluation hordeEval;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
+        hordeEval = GetComponent<HordeEvaluation>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        anim.SetBool("isClimbing", hordeEval.attemptLadderAvailable);
     }
 }
